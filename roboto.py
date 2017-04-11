16c34dbec6ba2caf2eab904547fd7c6b0f7e8c4b
Jameson Knoll <j.knoll621@gmail.com>
2017-04-11 15:27:14 +0800

    move api keys to config file

---
+++ roboto.py
@@ -0,0 +1,41 @@
+import twitter
+import yaml
+
+# get config from yml
+with open("config.yaml", 'r') as stream:
+    try:
+        config = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+# authenticate against twitter api
+api = twitter.Api(consumer_key=config["CONSUMER_KEY"],
+                  consumer_secret=config["CONSUMER_SECRET"],
+                  access_token_key=config["ACCESS_TOKEN"],
+                  access_token_secret=config["ACCESS_TOKEN_SECRET"])
+
+# favorites and rewteets - topic 1
+results = api.GetSearch(
+    raw_query="q=scholarship&count=5")
+
+for r in results:
+  api.CreateFavorite(r)
+
+results = api.GetSearch(
+  raw_query="q=scholarship&count=1")
+
+for r in results:
+  api.PostRetweet(r.id)
+
+# favorites and retweets - topic 2
+results = api.GetSearch(
+    raw_query="q=college&count=5")
+
+for r in results:
+  api.CreateFavorite(r)
+
+results = api.GetSearch(
+  raw_query="q=college&count=1")
+
+for r in results:
+  api.PostRetweet(r.id)
